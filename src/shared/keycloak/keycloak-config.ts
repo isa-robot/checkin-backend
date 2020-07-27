9e294e776be6ee4daa962dffeaa161c6a8b5cb28
erickpoleto <erick-poleto@hotmail.com>
2020-07-27 11:45:26 -0300

    keycloak atualizado

--- src/shared/keycloak/keycloak-config.ts
+++ src/shared/keycloak/keycloak-config.ts
@@ -6,15 +6,11 @@ class Keycloak {
   private keycloak: any;
   private keycloakConfig = {
     "realm": process.env.KEYCLOAK_REALM,
+    "bearer-only": true,
     "auth-server-url": `${process.env.KEYCLOAK_SERVER_URL}/`,
     "ssl-required": "external",
     "resource": process.env.KEYCLOAK_CLIENT,
-    "verify-token-audience": true,
-    "credentials": {
-      "secret": "dad5acb5-0550-469b-9497-e475afbe9672"
-    },
-    "confidential-port": 0,
-    "policy-enforcer": {}
+    "confidential-port": 0
   }
   constructor() {
     this.initKeycloak()
