d30b9dc40a124c89727b00a1192b37e2fd804e18
erickpoleto <erick-poleto@hotmail.com>
2020-10-08 18:08:10 -0300

    IQ-37 servico para listagem de dias para envio de email, servico de envio de emails

--- src/modules/protocols/cfpng/services/CreateCfpngService.ts
+++ src/modules/protocols/cfpng/services/CreateCfpngService.ts
@@ -61,8 +61,18 @@ class CreateCfpngService {
     if (!findProtocolActiveByNameByUser) {
       throw new AppError("protocol ativo nao encontrado", 404)
     }
-    if(new Date(data.protocolGenerationDate).toDateString() < findProtocolActiveByNameByUser.created_at.toDateString() ||
-      new Date(data.protocolGenerationDate).toDateString() > findProtocolActiveByNameByUser.protocolEndDate.toDateString()) {
+
+    const protocolGenerationDate = new DateHelper().dateToStringBR(new Date(data.protocolGenerationDate))
+    const protocolGenerationDateReverse = new Date(protocolGenerationDate.split("/").reverse().join("/"))
+
+    const protocolCreatedAt = new DateHelper().dateToStringBR(new Date(findProtocolActiveByNameByUser.created_at))
+    const protocolCreatedAtReverse = new Date(protocolCreatedAt.split("/").reverse().join("/"))
+
+    const protocolEndDate = new DateHelper().dateToStringBR(new Date(findProtocolActiveByNameByUser.protocolEndDate))
+    const protocolEndDateReverse = new Date(protocolEndDate.split("/").reverse().join("/"))
+
+    if(protocolGenerationDateReverse < protocolCreatedAtReverse ||
+      protocolGenerationDateReverse > protocolEndDateReverse) {
       throw new AppError("data de protocolo não se encontra no prazo", 404)
     }
 
