a23ee0939b797e88053766f4c5dae06ca9456929
Breno Scheffer <brenoscheffer@gmail.com>
2021-07-31 20:13:43 -0300

    IQ-78 Servico de download de arquivo

---
+++ src/shared/services/FileDownladerService.ts
@@ -0,0 +1,63 @@
+import https from 'https';
+import { ClientRequest, IncomingMessage } from 'http';
+import { injectable } from "tsyringe";
+import IFileDownloaderService from "./IFileDownloadService";
+import { Readable, Writable, Duplex } from 'stream';
+
+
+@injectable()
+export default class FileDownloaderService implements IFileDownloaderService {
+    constructor() { }
+
+    // Buffer|Uint8Array|Blob|string|Readable
+    download(url: string): any {
+        if (url) {
+            return this.agoraVai(url);
+        }
+        //todo better handle error
+        throw new Error("Must specify url");
+
+    }
+
+    private agoraVai(url: string): Promise<Buffer> {
+        let write: Buffer[] = [];        
+        return new Promise((resolve, reject) => {
+
+
+            https.get(url, function (response: IncomingMessage) {
+                if (response.statusCode === 200) {
+                    response.on('data', (data)=> {
+                        write.push(data);
+                    });
+                    response.on('end', ()=> {
+                        resolve(Buffer.concat(write));
+                    });
+                    // response.on('error', write.destroy)                 
+                } else {
+                    throw new Error(response.statusCode?.toString());
+                    // console.log('ERROR', response.statusCode)
+                }
+            }).on('error', (err: any) => {
+                console.log('ERROR', err)
+            })
+        });
+
+    }
+
+    private pleaseWork(url: string) {
+        return https.get(url, function (response: IncomingMessage) {
+            if (response.statusCode === 200) {
+                response.on('data', write.push);
+                response.on('end', console.log);
+                // response.on('error', write.destroy)                 
+            } else {
+                throw new Error(response.statusCode?.toString());
+                // console.log('ERROR', response.statusCode)
+            }
+        }).on('error', (err: any) => {
+            console.log('ERROR', err)
+        })
+    }
+
+
+}
